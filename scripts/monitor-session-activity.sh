5aaf420c7530199b102b2d377b676f2624f0e628
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-08-13 05:35:07 +0200

    Append hostname to bucket id

--- scripts/monitor-session-activity.sh
+++ scripts/monitor-session-activity.sh
@@ -15,7 +15,7 @@ HOST=$(get_tmux_option "@aw-watcher-tmux-host" "localhost")
 PORT=$(get_tmux_option "@aw-watcher-tmux-port" "5600")
 PULSETIME=$(get_tmux_option "@aw-watcher-tmux-pulsetime" "120.0")
 
-BUCKET_ID="aw-watcher-tmux"
+BUCKET_ID="aw-watcher-tmux_$(hostname)"
 API_URL="http://$HOST:$PORT/api"
 
 ######
